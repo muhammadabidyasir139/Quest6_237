d3f3e6cb745fe811cec1268be5231a1a87504c30
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-11-20 11:08:03 +0700

    perbaikan typo

--- app/src/main/java/com/example/viewmodel/view/TampilSiswa.kt
+++ app/src/main/java/com/example/viewmodel/view/TampilSiswa.kt
@@ -6,7 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.viewmodel.R
+import com.example.viewmodel.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,32 +23,46 @@ fun TampilSiswa(
         "Jenis Kelamin" to statusUiSiswa.gender,
         "Alamat" to statusUiSiswa.alamat
     )
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(id = R.string.data_siswa)) }
+                title = { Text(text = stringResource(id = R.string.data_siswa)) }
             )
         }
-    ) { padding ->
+    ) { paddingValues ->
 
         Column(
             modifier = Modifier
-                .padding(padding)
-                .padding(20.dp)
-        ){
+                .padding(paddingValues)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
 
             items.forEach { (label, value) ->
-                Column {
-                    Text(text = label.uppercase(), fontSize = 16.sp)
-                    Text(text = value, fontSize = 16.sp)
-                    Divider(thickness = dimensionResource(id = R.dimen._1dp))
+                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                    Text(
+                        text = label.uppercase(),
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = value,
+                        fontSize = 16.sp
+                    )
+                    Divider(
+                        thickness = dimensionResource(id = R.dimen._1dp)
+                    )
                 }
             }
+
             Spacer(modifier = Modifier.height(20.dp))
 
-            Button(onClick = onBackButtonClicked) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackButtonClicked
+            ) {
                 Text(text = stringResource(id = R.string.back))
             }
         }
     }
-    }
+}

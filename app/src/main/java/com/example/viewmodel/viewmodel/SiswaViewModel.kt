352e881afe3c8b2df9f7487bd7a162b0f5995c28
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-11-20 09:24:34 +0700

    create view model

--- app/src/main/java/com/example/viewmodel/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/viewmodel/viewmodel/SiswaViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.viewmodel.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-class SiswaViewModel : ViewModel {
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(value = Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls:MutableList<String>){
+        _statusUI.update {statusSaatIni ->
+            statusSaatIni.copy(nama=ls[0], gender =ls[1], alamat=ls[2])
+        }
+    }
 }
